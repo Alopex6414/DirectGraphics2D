57bfa5666a250213418344106bc3eb6476023bfe
Helium <1029535012@qq.com>
2017-11-07 16:58:17 +0800

    DirectGraphics2D DLL

---
+++ DirectGraphics2D/DirectGraphics2D.cpp
@@ -0,0 +1,11 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017, Alopex/Helium
+*     All rights reserved.
+*
+* @file		DirectGraphics2D.cpp
+* @brief	This Program is DirectGraphics2D DLL Project.
+* @author	Alopex/Helium
+* @version	v1.00a
+* @date		2017-11-2
+*/
---
+++ DirectGraphics2D/DirectGraphics2D.h
@@ -0,0 +1,19 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017, Alopex/Helium
+*     All rights reserved.
+*
+* @file		DirectGraphics2D.h
+* @brief	This Program is DirectGraphics2D DLL Project.
+* @author	Alopex/Helium
+* @version	v1.00a
+* @date		2017-11-2
+*/
+#pragma once
+
+#ifndef __DIRECTGRAPHICS2D_H_
+#define __DIRECTGRAPHICS2D_H_
+
+
+
+#endif
